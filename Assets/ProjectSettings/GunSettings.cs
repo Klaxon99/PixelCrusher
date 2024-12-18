f540b3b3e646d8c8214f7c739a3e119fa39a79f5
Maksim <maxloda2013@yandex.ru>
2024-12-18 17:06:23 +0500

    all

---
+++ Assets/ProjectSettings/GunSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunSettings", menuName = "Settings/GunSettings", order = 51)]
+public class GunSettings : ScriptableObject
+{
+    [SerializeField] private GunView _gunPrefab;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _reloadTime;
+
+    public float Speed => _speed;
+
+    public float ReloadTime => _reloadTime;
+
+    public GunView GunPrefab => _gunPrefab;
+}
---
+++ Assets/ProjectSettings/ISoundSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public interface ISoundSwitcher
+{
+    public event Action Muted;
+
+    public event Action Unmuted;
+
+    public void Mute();
+
+    public void Unmute();
+}
